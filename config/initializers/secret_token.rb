0ccb8ed9e676d7c1785d0e33840540755e3dbe02
ncai86 <nicholascai86@gmail.com>
2013-04-25 00:35:02 +0800

    scaffold files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Possim::Application.config.secret_token = 'b17c8afc94e55352d4cb3579b4fab1d9ecb962661c1153bc3c31010e1059f7d4cae83a02616410be9bcd6520d369c7b58f4c99bdcc12c8ad4a9dda3ed1488678'
