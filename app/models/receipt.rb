0bd4ccd0807dde0ef46939189f4b00d809acc749
ncai86 <nicholascai86@gmail.com>
2013-05-03 16:19:17 +0800

    added dependent destroy, create_items method to receipts

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,4 +1,18 @@
 class Receipt < ActiveRecord::Base
-  has_many :purchase_items
+  has_many :purchase_items, :dependent => :destroy
   attr_accessible :detailed_description, :good_description, :gross_amount, :is_luxury, :purchase_date_time, :receipt_number, :quantity
+
+#  instance methods
+  def create_items(item_count)
+    item_count.times do
+      self.purchase_items.create(
+          good_description: Faker::Lorem.word,
+          detailed_description: Faker::Lorem.sentence,
+          quantity: Random.rand(0..9),
+          gross_amount: Random.rand(1000..10000),
+          vat_rate: Random.rand(1.00..10.00).round(2),
+          is_luxury: Random.rand(0..1)
+      )
+    end
+  end
 end
