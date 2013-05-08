b60f1894112913a288a4a66c1151fe6a219e764d
ncai86 <nicholascai86@gmail.com>
2013-05-08 16:29:06 +0800

    allow dynamic checking valid_parameters, data type and length, with specs

--- spec/controllers/receipts_controller_spec.rb
+++ spec/controllers/receipts_controller_spec.rb
@@ -20,7 +20,7 @@ describe ReceiptsController do
     @receipt = create(:receipt, :receipt_number => '1234567891234567891234567890')
   end
 
-  context 'XML POST REQUEST' do
+  describe 'XML POST REQUEST' do
     it 'returns response successfully' do
       #request.env['RAW_POST_DATA'] = post_params.to_xml(:root => 'ReceiptRequest')
       #Rails.logger.info request.env['RAW_POST_DATA']
@@ -56,7 +56,6 @@ describe ReceiptsController do
 
     it 'renders error template with 01 error code when length is not valid' do
       post_params[:ReceiptRequest][:ReceiptNumber] = post_params[:ReceiptRequest][:ReceiptNumber] + '1'
-      debugger
 
       post :get_receipt, post_params.merge(:format => :xml)
       expect(response).to be_success
@@ -71,6 +70,42 @@ describe ReceiptsController do
       expect(response).to render_template("error")
     end
 
+    context "post params node differ from VALID_PARAMETERS" do
+
+      it 'returns error response when request contains differing node names ' do
+        post_params = {
+            :ReceiptRequest =>
+                {
+                    :BadReceiptNumber => '1234567891234567891234567890',
+                    :BadGoodsType => 1
+                }
+        }
+
+        post :get_receipt, post_params.merge(:format => :xml)
+        expect(response).to be_success
+        expect(response).to render_template("error")
+      end
+
+      it 'returns error response when request contains differing number of nodes' do
+        post_params[:ReceiptRequest].merge! :ExtraNode => 'extra node added'
+        post :get_receipt, post_params.merge(:format => :xml)
+        expect(response).to be_success
+        expect(response).to render_template("error")
+      end
+    end
+
+    context "VALID_PARAMETERS length not filled in optional" do
+      it "returns response successfully" do
+        stub_const("VALID_PARAMETERS", [
+            {:param_name => "ReceiptNumber", :class => String},
+            {:param_name => "GoodsType", :class => Fixnum}
+        ])
+
+        post :get_receipt, post_params.merge(:format => :xml)
+        expect(response).to be_success
+        expect(response).to render_template("get_receipt")
+      end
+    end
 
   end
 
