1925fe202affb12fb8014e47e9b080c220ce69fd
Nicholas <nicholascai86@gmail.com>
2013-05-10 11:03:01 +0800

    rearrange/reformat seed data and constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -16,10 +16,16 @@ VALID_PARAMETERS = [
                     ]
 
 # Parameters that are present in the request but not needed/checked
+# DO NOT remove 'xmlns:xsi', 'xmlns:xsd' EVER!
 REDUNDANT_REQUEST_PARAMS = ["xmlns:xsi", "xmlns:xsd", "CorpID", "StoreID"]
 
 # Nodes of purchase items present/wanted in the response xml
-RESPONSE_NODES = ['GoodDescription', 'DetailedDescription', 'Quantity', 'GrossAmount', 'VatRate', 'IsLuxury']
+RESPONSE_NODES = ['GoodDescription',
+				  'DetailedDescription',
+				  'Quantity',
+				  'GrossAmount',
+				  'VatRate',
+				  'IsLuxury']
 
 # Query assigning the array of purchase_items -- @purchase_items 
 QUERY_STRING = "@receipt = Receipt.find_by_receipt_number(@receipt_number); (luxury_filter.present? ? @receipt.purchase_items.where(:is_luxury => luxury_filter) : @receipt.purchase_items) if @receipt"
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@
 ##### POS Simulator Test Data Sets
 Receipt.destroy_all
 receipt = Receipt.create receipt_number: '02111112590010007000194', purchase_date_time: DateTime.now.strftime("%FT%T")
-receipt.purchase_items.create(good_description: 'Watch',  detailed_description: 'Omega', quantity: 1, gross_amount: 10500, vat_rate: 5.00, is_luxury: 1)
+receipt.purchase_items.create(good_description: 'Watch',  detailed_description: 'Omega', quantity: 1, gross_amount: 10500, vat_rate: 10.00, is_luxury: 0)
 receipt.purchase_items.create(good_description: Faker::Lorem.word,
                               detailed_description: Faker::Lorem.sentence,
-                              quantity: Random.rand(0..9),
+                              quantity: Random.rand(1..9),
                               gross_amount: Random.rand(1000..10000),
-                              vat_rate: Random.rand(1.00..10.00).round(2),
-                              is_luxury: Random.rand(0..1))
+                              vat_rate: 10.00,
+                              is_luxury: 0)
